3fe318d4284f28163f37193ed5fe42edc652f8fb
daejoon <kkode1911@gmail.com>
2026-02-13 23:03:39 +0900

    fix: 이전 com.github 패키지 경로의 잔존 파일 제거
    빌드 과정에서 재생성된 이전 패키지 경로 파일을 삭제.
    
    고대준 <daejoon.ko@hanatour.com>

--- src/main/java/com/github/direnvloader/DirenvBlockedException.java
+++
@@ -1,8 +0,0 @@
-package com.github.direnvloader;
-
-// .envrc가 blocked 상태일 때 발생하는 예외
-public class DirenvBlockedException extends DirenvException {
-    public DirenvBlockedException(String message) {
-        super(message);
-    }
-}
