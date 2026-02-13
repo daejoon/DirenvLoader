51f7d8c26ba01dcb3f30d089bc6db9cd9474bcbd
daejoon <kkode1911@gmail.com>
2026-02-13 21:54:54 +0900

    fix: DirenvNotifierTest를 JUnit 5로 변환하여 IU 호환성 확보
    고대준 <daejoon.ko@hanatour.com>

--- src/test/java/com/github/direnvloader/DirenvNotifierTest.java
+++ src/test/java/com/github/direnvloader/DirenvNotifierTest.java
@@ -2,12 +2,16 @@ package com.github.direnvloader;
 
 import com.intellij.notification.Notification;
 import com.intellij.notification.NotificationType;
-import com.intellij.testFramework.fixtures.BasePlatformTestCase;
+import org.junit.jupiter.api.Test;
 
-public class DirenvNotifierTest extends BasePlatformTestCase {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class DirenvNotifierTest {
 
     // direnv 미설치 알림이 WARNING 타입인지 확인
-    public void testNotifyNotInstalled() {
+    @Test
+    void notifyNotInstalled() {
         Notification notification = DirenvNotifier.createNotInstalledNotification();
 
         assertEquals("Direnv Loader", notification.getGroupId());
@@ -16,7 +20,8 @@ public class DirenvNotifierTest extends BasePlatformTestCase {
     }
 
     // .envrc blocked 알림이 WARNING 타입인지 확인
-    public void testNotifyBlocked() {
+    @Test
+    void notifyBlocked() {
         Notification notification = DirenvNotifier.createBlockedNotification();
 
         assertEquals("Direnv Loader", notification.getGroupId());
@@ -25,7 +30,8 @@ public class DirenvNotifierTest extends BasePlatformTestCase {
     }
 
     // 일반 오류 알림이 ERROR 타입이고 메시지를 포함하는지 확인
-    public void testNotifyError() {
+    @Test
+    void notifyError() {
         String message = "timeout exceeded";
         Notification notification = DirenvNotifier.createErrorNotification(message);
 
